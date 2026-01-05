0da271aad157a10bc28e02b7de16dbefbe9bf33c
unqYatharth <yatharth.varshney1012@gmail.com>
2026-01-05 22:45:27 +0530

    html done and addTodo fn created

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Todo App")
+const inputElem = document.getElementById("input-el")
+const addBtn = document.getElementById("add-btn")
+const ulElem = document.getElementById("ul-el")
+
+const todos = []
+
+addBtn.addEventListener('click', addTodo)
+
+function addTodo() {
+  if(!inputElem.value) return
+  
+  todos.push(
+    {
+      id: Date.now(),
+      text: inputElem.value,
+      isCompleted: false
+    }
+  )
+  inputElem.value = ""
+}
+
